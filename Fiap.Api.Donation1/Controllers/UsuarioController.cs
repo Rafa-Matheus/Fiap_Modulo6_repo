492ccc6eb9dd31107d7be507f55bf35330e97133
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-10-04 18:44:45 -0300

    Create UsuarioController.cs

---
+++ Fiap.Api.Donation1/Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using Fiap.Api.Donation1.Models;
+using Fiap.Api.Donation1.Repository.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Api.Donation1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository usuarioRepository;
+
+        public UsuarioController(IUsuarioRepository _usuarioRepository)
+        {
+            usuarioRepository = _usuarioRepository;
+        }
+
+        [HttpPost] // Para proteger os dados sensíveis do usuário
+        [Route("Login")]
+        public UsuarioModel Login([FromBody] UsuarioModel usuarioModel) 
+        {
+            var usuario = usuarioRepository
+                .FindByEmailAndSenha(usuarioModel.EmailUsuario, usuarioModel.Senha);
+
+            return usuario;
+        }
+
+        [HttpGet]
+        public IList<UsuarioModel> Get()
+        {
+            var usuarios = usuarioRepository.FindAll();
+
+            return usuarios;
+        }
+    }
+}
