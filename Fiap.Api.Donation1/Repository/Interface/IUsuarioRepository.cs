eaeb851fd34a6fb3b79478adf62b8ea370352c26
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-10-04 18:45:35 -0300

    Create IUsuarioRepository.cs

---
+++ Fiap.Api.Donation1/Repository/Interface/IUsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using Fiap.Api.Donation1.Models;
+
+namespace Fiap.Api.Donation1.Repository.Interface
+{
+    public interface IUsuarioRepository
+    {
+        public UsuarioModel FindByEmailAndSenha(string email, string senha);
+
+        public UsuarioModel FindById(int id);
+
+        public IList<UsuarioModel> FindAll();
+
+        public void Delete(int id);
+    }
+}
