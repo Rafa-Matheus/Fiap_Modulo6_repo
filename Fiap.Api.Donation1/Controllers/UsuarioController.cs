8a32aa7e06126ce4099d308a413d48561ece7af6
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-10-05 18:50:59 -0300

    Updated and corrected UsuarioController.cs

--- Fiap.Api.Donation1/Controllers/UsuarioController.cs
+++ Fiap.Api.Donation1/Controllers/UsuarioController.cs
@@ -1,7 +1,9 @@
 ﻿using Fiap.Api.Donation1.Models;
 using Fiap.Api.Donation1.Repository.Interface;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
 
 namespace Fiap.Api.Donation1.Controllers
 {
@@ -18,20 +20,92 @@ namespace Fiap.Api.Donation1.Controllers
 
         [HttpPost] // Para proteger os dados sensíveis do usuário
         [Route("Login")]
-        public UsuarioModel Login([FromBody] UsuarioModel usuarioModel) 
+        public ActionResult<UsuarioModel> Login([FromBody] UsuarioModel usuarioModel) 
         {
             var usuario = usuarioRepository
                 .FindByEmailAndSenha(usuarioModel.EmailUsuario, usuarioModel.Senha);
 
-            return usuario;
+            if (usuario != null) return Ok();
+            else return NotFound();
+
+            //return usuario;
         }
 
         [HttpGet]
-        public IList<UsuarioModel> Get()
+        public ActionResult<IList<UsuarioModel>> Get()
         {
             var usuarios = usuarioRepository.FindAll();
 
-            return usuarios;
+            if (usuarios == null || usuarios.Count == 0) return NoContent();
+            else return Ok(usuarios);
+
+            //return usuarios;
         }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<UsuarioModel> Get(int id)
+        {
+            var usuario = usuarioRepository.FindById(id);
+
+            if (usuario == null || usuario.UsuarioId == 0) return NotFound();
+            else return Ok(usuario);
+
+            //return usuarioRepository.FindById(id);
+        }
+
+        [HttpPost]
+        public ActionResult<UsuarioModel> Post([FromBody] UsuarioModel usuarioModel)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            /*var retorno = usuarioRepository.Insert(usuarioModel);
+
+            return Created("",retorno);*/
+
+            try
+            {
+
+                usuarioRepository.Insert(usuarioModel);
+
+                var url = Request.GetEncodedUrl().EndsWith("/") ?
+                                                Request.GetEncodedUrl() :
+                                                Request.GetEncodedUrl() + "/";
+
+                var location = new Uri(url + usuarioModel.UsuarioId);
+
+                return Created(location, usuarioModel);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<UsuarioModel> Put([FromRoute] int id, [FromBody] UsuarioModel usuarioModel)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            if (id != usuarioModel.UsuarioId) return NotFound();
+
+            usuarioRepository.Update(usuarioModel);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<int> Delete([FromRoute] int id)
+        {
+            if (id == 0) return BadRequest();
+
+            var usuarioModel = usuarioRepository.FindById(id);
+
+            if (usuarioModel == null) return NotFound();
+            
+            usuarioRepository.Delete(id);
+            return NoContent();
+            
+        }
+
     }
 }
